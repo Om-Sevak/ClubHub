5bc3cf86bfdcb170c8022e4ecaa5c1a6e04791f8
itay <nudeli@myumanitoba.ca>
2024-02-25 14:27:01 -0600

    Implemented leave club functionallity

--- client/src/api/clubRole.js
+++ client/src/api/clubRole.js
@@ -2,7 +2,8 @@ import sendRequest from "./client";
 
 const clubRoleApi = {
   getClubRole: async (clubName) => sendRequest('GET', `role/${clubName}`),
-  createClubRole: async (clubName, roleData) => sendRequest('POST', `role/${clubName}`, roleData)
+  createClubRole: async (clubName, roleData) => sendRequest('POST', `role/${clubName}`, roleData),
+  deleteClubRole: async (clubName) => sendRequest('DELETE', `role/${clubName}`)
 };
 
 // Club API functions
