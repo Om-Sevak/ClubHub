3726ac8d784635ab04e5ece8842559581a4e4a0a
itay <nudeli@myumanitoba.ca>
2024-02-25 12:31:19 -0600

    created test for creating a club

---
+++ server/test/clubAPI.test.js
@@ -0,0 +1,52 @@
+const request = require("supertest");
+var session = require('supertest-session');
+const app = require('../app');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+let mongoServer;
+let testSession = null;
+
+describe('Club Endpoints', () => {
+
+    beforeAll(async () => {
+        mongoServer = await MongoMemoryServer.create();
+        const mongoUri = mongoServer.getUri();
+        await mongoose.connect(mongoUri);
+
+        await User.deleteMany();
+            // Create a user
+            const user = new User({
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'john@example.com',
+                passwordHash: await bcrypt.hash('password', 12)
+            });
+        await user.save();
+    });
+    
+    afterAll(async () => {
+        await mongoose.disconnect();
+        await mongoServer.stop();
+    });
+
+    describe('POST /club', () => {
+        test('Should create a new club succesfully', function (done) {
+
+            testSession = session(app);
+
+            testSession.post('/login')
+                .send({ email: 'john@example.com', password: 'password' })
+                .expect(200)
+                .end(done)
+
+            // This returns a 403, the session does not persist
+            testSession.post('/club')
+                .send({name: 'test club', descirption: 'Test description', email: 'johnny@example.com'})
+                .expect(200)
+                .end(done)
+        });
+    })
+})
