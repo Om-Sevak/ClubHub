20da57f91d4abd26bf022f23f5eb54f0fb801fc1
itay <nudeli@myumanitoba.ca>
2024-02-26 22:43:02 -0600

    created tests for club controller

--- server/test/clubAPI.test.js
+++ server/test/clubAPI.test.js
@@ -9,6 +9,7 @@ const bcrypt = require('bcryptjs');
 let mongoServer;
 let testSession = null;
 
+
 describe('Club Endpoints', () => {
 
     beforeAll(async () => {
@@ -24,7 +25,16 @@ describe('Club Endpoints', () => {
                 email: 'john@example.com',
                 passwordHash: await bcrypt.hash('password', 12)
             });
+
+            const anotherUser = new User({
+                firstName: 'test',
+                lastName: 'test',
+                email: 'test@example.com',
+                passwordHash: await bcrypt.hash('password', 12)
+            });
+
         await user.save();
+        await anotherUser.save();
     });
     
     afterAll(async () => {
@@ -37,14 +47,148 @@ describe('Club Endpoints', () => {
 
             testSession = session(app);
 
-            const res = await testSession.post('/login')
+            const loginRes = await testSession.post('/login')
                 .send({ email: 'john@example.com', password: 'password' })
                 .expect(200);
 
-            // This returns a 403, the session does not persist
-            const resone = await testSession.post('/club')
+            const createClubRes = await testSession.post('/club')
                 .send({name: 'test club', descirption: 'Test description', email: 'johnny@example.com'})
                 .expect(200);
+        });
+
+        test('Should fail to create a new club if a user is not signed in', async () => {
+
+            testSession = session(app);
+
+            const createClubRes = await testSession.post('/club')
+                .send({name: 'test club', descirption: 'Test description', email: 'johnny@example.com'})
+                .expect(403);
+        });
+
+        test('Should fail to create a new club if a club with the requested name already exists', async () => {
+
+            testSession = session(app);
+
+            const loginRes = await testSession.post('/login')
+                .send({ email: 'john@example.com', password: 'password' })
+                .expect(200);
+
+            const createClubFail = await testSession.post('/club')
+                .send({name: 'test club', descirption: 'Test description', email: 'johnny@example.com'})
+                .expect(400);
+        });
+
+        test('Should fail to create a new club if the email format provided is incorrect', async () => {
+
+            testSession = session(app);
+
+            const loginRes = await testSession.post('/login')
+                .send({ email: 'john@example.com', password: 'password' })
+                .expect(200);
+
+            const createClubFail = await testSession.post('/club')
+                .send({name: 'test', descirption: 'Test description', email: 'johnnyexample.com'})
+                .expect(400);
+        });
+    })
+
+    describe('GET /club/:clubName', () => {
+        
+        test('Should get club successfully', async () => {
+
+            testSession = session(app);
+
+            const getClubRes = await testSession.get('/club/test club')
+                .expect(200);
+        });
+
+        test('Should fail to get club that doesnt exist', async () => {
+
+            testSession = session(app);
+
+            const getClubRes = await testSession.get('/club/Dne club')
+                .expect(404);
+        });
+    })
+
+    describe('PUT /club/:clubName', () => {
+        
+        test('Should edit club successfully', async () => {
+
+            testSession = session(app);
+
+            const loginRes = await testSession.post('/login')
+                .send({ email: 'john@example.com', password: 'password' })
+                .expect(200);
+
+            const editClubRes = await testSession.put('/club/test club')
+                .send({name: 'test club', descirption: 'testing editing', email: 'johnny@example.ca'})
+                .expect(201);
+        });
+
+        test('Should fail to edit club name if the email format is invalid', async () => {
+
+            testSession = session(app);
+
+            const loginRes = await testSession.post('/login')
+                .send({ email: 'john@example.com', password: 'password' })
+                .expect(200);
+
+                const editClubRes = await testSession.put('/club/test club')
+                .send({name: 'test club', descirption: 'testing editing', email: 'johnny@example'})
+                .expect(400);
+        });
+
+        test('Should fail to edit club name if the club name already exists', async () => {
+
+            testSession = session(app);
+
+            const loginRes = await testSession.post('/login')
+                .send({ email: 'john@example.com', password: 'password' })
+                .expect(200);
+
+            const createClubRes = await testSession.post('/club/')
+                .send({name: 'new club', descirption: 'testing editing', email: 'johnny@example.ca'})
+                .expect(200);
+            
+            const editClubRes = await testSession.put('/club/test club')
+                .send({name: 'new club', descirption: 'testing editing', email: 'johnny@example.ca'})
+                .expect(400);
+        });
+
+        test('Should fail to edit a club that does not exist', async () => {
+
+            testSession = session(app);
+
+            const loginRes = await testSession.post('/login')
+                .send({ email: 'john@example.com', password: 'password' })
+                .expect(200);
+
+            const editClubRes = await testSession.put('/club/fake club')
+                .send({name: 'fake club', descirption: 'testing editing', email: 'johnny@example.com'})
+                .expect(404);
+        });
+
+        test('Should fail to edit if the user is not signed in', async () => {
+
+            testSession = session(app);
+
+            const editClubRes = await testSession.put('/club/test club')
+                .send({name: 'test club', descirption: 'testing editing', email: 'johnny@example.com'})
+                .expect(403);
+        });
+
+        test('Should fail to edit if the user is not an admin', async () => {
+
+            testSession = session(app);
+
+            const loginRes = await testSession.post('/login')
+                .send({ email: 'test@example.com', password: 'password' })
+                .expect(200);
+
+            const editClubRes = await testSession.put('/club/test club')
+                .send({name: 'test club', descirption: 'testing editing', email: 'johnny@example.com'})
+                .expect(403);
         });
     })
 })
