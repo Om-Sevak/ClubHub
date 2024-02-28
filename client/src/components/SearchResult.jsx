c52c592ee7c24c855f2899f0f2fed5603e05ce9c
swermie1 <swermie56@hotmail.com>
2024-02-27 18:35:11 -0600

    reload page on result select for search

--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -2,11 +2,17 @@ import "./SearchResult.css";
 import { useNavigate } from 'react-router-dom';
 
 export const SearchResult = ({ result }) => {
-    const navigate = useNavigate();
+    
+  const handleResultClick = () => {
+    navigate(`/club/${result}`)
+    window.location.reload();
+  }
+  
+  const navigate = useNavigate();
     return (
     <div
       className="search-result"
-      onClick={(e) => navigate(`/club/${result}`)}
+      onClick={handleResultClick}
     >
       {result}
     </div>
