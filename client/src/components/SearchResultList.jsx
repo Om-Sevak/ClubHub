eb4e9ef06057cf0256bef031afd14cb2698409ec
swermie1 <swermie56@hotmail.com>
2024-02-27 18:08:54 -0600

    create-club-page header and bugfix

--- client/src/components/SearchResultList.jsx
+++ client/src/components/SearchResultList.jsx
@@ -3,10 +3,12 @@ import { SearchResult } from "./SearchResult";
 
 export const SearchResultsList = ({ results }) => {
   return (
-    <div className="search-results-list">
-      {results.map((result, id) => {
-        return <SearchResult result={result} key={id} />;
-      })}
+    <div className="search-results-container">
+      <div className="search-results-list">
+        {results.map((result, id) => {
+          return <SearchResult result={result} key={id} />;
+        })}
+      </div>
     </div>
   );
-};
+};  
--- client/src/pages/createClubPage.js
+++ client/src/pages/createClubPage.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import './createClubPage.css'; // Import CSS file for styling
 import clubApi from '../api/clubs';
+import Header from '../components/Header';
 
 
 const ClubCreatePage = () => {
@@ -32,57 +33,60 @@ const ClubCreatePage = () => {
         
     return (
         <div className="create-club-page">
-          <div className="create-club-container">
-            <h2>Let's create a new club!</h2>
-            <form onSubmit={handleclubcreate}>
-              {/* Add labels for input fields */}
-              <label htmlFor="clubname">
-                Club Name:
-                <input
-                  id="clubname"
-                  type="text"
-                  placeholder="Enter the name of the club"
-                  value={clubname}
-                  onChange={(e) => setClubName(e.target.value)}
-                />
-              </label>
-    
-              <label htmlFor="clubdescription">
-                Club Description:
-                <input
-                  id="clubdescription"
-                  type="text"
-                  placeholder="Enter the description for the club"
-                  value={clubdescription}
-                  onChange={(e) => setClubDescription(e.target.value)}
-                />
-              </label>
-    
-              <label htmlFor="clubinterest">
-                Club Interests:
-                <input
-                  id="clubinterest"
-                  type="text"
-                  placeholder="Enter the interests for the club"
-                  value={clubinterest}
-                  onChange={(e) => setClubInterest(e.target.value)}
-                />
-              </label>
-    
-              <label htmlFor="clubemail">
-                Club Email:
-                <input
-                  id="clubemail"
-                  type="text"
-                  placeholder="Enter the club email"
-                  value={clubemail}
-                  onChange={(e) => setClubEmail(e.target.value)}
-                />
-              </label>
-    
-              <button type="submit">Create</button>
-            </form>
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <Header />
+          <div className="create-club-col">
+            <div className="create-club-container">
+              <h2>Let's create a new club!</h2>
+              <form onSubmit={handleclubcreate}>
+                {/* Add labels for input fields */}
+                <label htmlFor="clubname">
+                  Club Name:
+                  <input
+                    id="clubname"
+                    type="text"
+                    placeholder="Enter the name of the club"
+                    value={clubname}
+                    onChange={(e) => setClubName(e.target.value)}
+                  />
+                </label>
+      
+                <label htmlFor="clubdescription">
+                  Club Description:
+                  <input
+                    id="clubdescription"
+                    type="text"
+                    placeholder="Enter the description for the club"
+                    value={clubdescription}
+                    onChange={(e) => setClubDescription(e.target.value)}
+                  />
+                </label>
+      
+                <label htmlFor="clubinterest">
+                  Club Interests:
+                  <input
+                    id="clubinterest"
+                    type="text"
+                    placeholder="Enter the interests for the club"
+                    value={clubinterest}
+                    onChange={(e) => setClubInterest(e.target.value)}
+                  />
+                </label>
+      
+                <label htmlFor="clubemail">
+                  Club Email:
+                  <input
+                    id="clubemail"
+                    type="text"
+                    placeholder="Enter the club email"
+                    value={clubemail}
+                    onChange={(e) => setClubEmail(e.target.value)}
+                  />
+                </label>
+      
+                <button type="submit">Create</button>
+              </form>
+              {errorMessage && <p className="error-message">{errorMessage}</p>}
+            </div>
           </div>
         </div>
       );
